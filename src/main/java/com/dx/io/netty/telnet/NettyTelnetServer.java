02f7b98891130cfa079ceb9a298bc74fc66058a0
dengxu <mail.deng@qq.com>
2019-10-29 17:42:19 +0800

    Netty 创建Telnet服务

---
+++ src/main/java/com/dx/io/netty/telnet/NettyTelnetServer.java
@@ -0,0 +1,56 @@
+package com.dx.io.netty.telnet;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author : dengxu
+ * @create 2019-10-29 16:45
+ **/
+public class NettyTelnetServer {
+
+    public static void main(String[] args) {
+
+        final int PORT = 8888;
+        ServerBootstrap serverBootstrap;
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+                            pipeline.addLast(new StringDecoder());
+                            pipeline.addLast(new StringEncoder());
+                            pipeline.addLast(new NettyTelnetHandle());
+                        }
+                    });
+
+            Channel channel = serverBootstrap.bind(PORT).sync().channel();
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
